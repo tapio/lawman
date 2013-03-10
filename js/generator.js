c65f3c66499c2234dd475bf3f3dbd537dc44ef91
Tapio Vierros <tapiovierros@gmail.com>
2013-03-10 17:02:32 +0200

    House skeletons.

--- js/generator.js
+++ js/generator.js
@@ -3,14 +3,19 @@ function generateTown(map) {
 	var tiles = map.map;
 	var w = map.width;
 	var h = map.height;
+	var cy = Math.floor(h / 2);
 
 	var WALL = new ut.Tile('#', 100, 100, 100);
-	var FLOOR = new ut.Tile('.', 50, 50, 50);
+	var DOOR = new ut.Tile('+', 110, 30, 0);
+	var FLOOR = new ut.Tile('.', 150, 140, 80);
 	var DIRT = new ut.Tile('.', 80, 50, 0);
 	var ROAD = new ut.Tile('.', 80, 70, 50);
 	var MOUNTAIN = new ut.Tile('^', 120, 120, 120);
 
 	var r = new Alea(42);
+	function rand(lo, hi) {
+		return lo + Math.floor(r.random() * (hi - lo + 1));
+	}
 
 	// Fill with dirt
 	map.fill(DIRT, 0, 0, w, h);
@@ -33,7 +38,40 @@ function generateTown(map) {
 	})();
 
 	// Main road
-	map.fill(ROAD, 4, Math.floor(h / 2) - 4, w - 8, 8);
+	var roadWidth = 8;
+	map.fill(ROAD, 4, cy - roadWidth / 2, w - 8, roadWidth);
+
+	// Houses
+	(function() {
+		var margin = 10;
+		var lotSize = 15;
+
+		function randomHouse(facing) {
+			var house = {
+				w: rand(8, lotSize-2),
+				h: rand(8, lotSize-2),
+				facing: facing
+			};
+			house.x = Math.floor((lotSize - house.w) / 2);
+			house.y = Math.floor((lotSize - house.h) / 2);
+			return house;
+		}
+
+		function createHouseFrame(house, lotX, lotY) {
+			map.fill(FLOOR, lotX + house.x + 1, lotY + house.y + 1, house.w - 2, house.h - 2);
+			map.border(WALL, lotX + house.x, lotY + house.y, house.w, house.h);
+			var doorX = lotX + house.x + Math.floor(house.w / 2);
+			var doorY = house.facing === "up" ? lotY + house.y : lotY + house.y + house.h - 1;
+			tiles[doorY][doorX] = DOOR;
+		}
+
+		var houses = Math.floor((w - 2 * margin) / lotSize);
+		for (var i = 0; i < houses; ++i) {
+			var lotX = margin + i * lotSize;
+			createHouseFrame(randomHouse("down"), lotX, cy - lotSize - roadWidth / 2 - 1);
+			createHouseFrame(randomHouse("up"), lotX, cy + roadWidth / 2 + 1);
+		}
+	})();
 
 	return map;
 }
--- js/map.js
+++ js/map.js
@@ -1,6 +1,6 @@
 function Map() {
 	this.width = 100;
-	this.height = 50;
+	this.height = 80;
 	this.map = new Array(this.height);
 	for (var j = 0; j < this.height; ++j)
 		this.map[j] = new Array(this.width);
@@ -13,11 +13,11 @@ Map.prototype.fill = function(tile, x, y, w, h) {
 };
 
 Map.prototype.border = function(tile, x, y, w, h) {
-	for (var j = 0; j < h; ++j) {
+	for (var j = y; j < y + h; ++j) {
 		this.map[j][x] = tile;
 		this.map[j][x+w-1] = tile;
 	}
-	for (var i = 0; i < w; ++i) {
+	for (var i = x; i < x + w; ++i) {
 		this.map[y][i] = tile;
 		this.map[y+h-1][i] = tile;
 	}
