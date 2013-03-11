b921bee9acfcf89c1c29dcc76bca724aca1065d3
Tapio Vierros <tapiovierros@gmail.com>
2013-03-11 15:17:37 +0200

    Visualize gun UI.

--- js/ui.js
+++ js/ui.js
@@ -8,24 +8,34 @@ function UI(view, pl) {
 		return st;
 	}
 
+	function pickColor(gun, pl) {
+		if (!gun.damage) return { r: 64, g: 64, b: 64 };
+		if (pl.drawn == gun) return { r: 160, g: 0, b: 0 };
+		return { r: 160, g: 100, b: 64 };
+	}
+
 	this.update = function() {
-		var row = 0;
+		var row = 0, c;
 		view.putString("Health:", 0, row, 160, 0, 0);
 		view.putString(build("♥", pl.health, "♡", pl.maxHealth), 8, row++, 255, 0, 0);
 		view.putString("Money: $" + pl.money, 0, row++, 200, 200, 0);
 		++row;
-		view.putString("[1] Right hand:", 0, row++, 128, 128, 128);
-		view.putString(" " + pl.weapons.gun1.name, 0, row++, 160, 0, 0);
-		view.putString(" Ammo: " + build("✒", pl.weapons.gun1.ammo), 0, row++, 160, 0, 0);
-		view.putString("[1] Left hand:", 0, row++, 128, 128, 128);
-		view.putString(" " + pl.weapons.gun2.name, 0, row++, 160, 0, 0);
-		view.putString(" Ammo: " + build("✒", pl.weapons.gun2.ammo), 0, row++, 160, 0, 0);
-		view.putString("[2] Secondary:", 0, row++, 128, 128, 128);
-		view.putString(" " + pl.weapons.secondary.name, 0, row++, 160, 0, 0);
-		view.putString(" Ammo: " + build("✒", pl.weapons.secondary.ammo), 0, row++, 160, 0, 0);
-		view.putString("[3] Throwable:", 0, row++, 128, 128, 128);
-		view.putString(" " + pl.weapons.throwable.name, 0, row++, 160, 0, 0);
-		view.putString(" Ammo: " + build("✒", pl.weapons.throwable.ammo), 0, row++, 160, 0, 0);
+		c = pickColor(pl.weapons.gun1, pl);
+		view.putString("[1] Right hand:", 0, row++, c.r, c.g, c.b);
+		view.putString(" " + pl.weapons.gun1.name, 0, row++, c.r, c.g, c.b);
+		view.putString(" Ammo: " + build("✒", pl.weapons.gun1.ammo), 0, row++, c.r, c.g, c.b);
+		c = pickColor(pl.weapons.gun2, pl);
+		view.putString("[1] Left hand:", 0, row++, c.r, c.g, c.b);
+		view.putString(" " + pl.weapons.gun2.name, 0, row++, c.r, c.g, c.b);
+		view.putString(" Ammo: " + build("✒", pl.weapons.gun2.ammo), 0, row++, c.r, c.g, c.b);
+		c = pickColor(pl.weapons.secondary, pl);
+		view.putString("[2] Secondary:", 0, row++, c.r, c.g, c.b);
+		view.putString(" " + pl.weapons.secondary.name, 0, row++, c.r, c.g, c.b);
+		view.putString(" Ammo: " + build("✒", pl.weapons.secondary.ammo), 0, row++, c.r, c.g, c.b);
+		c = pickColor(pl.weapons.throwable, pl);
+		view.putString("[3] Throwable:", 0, row++, c.r, c.g, c.b);
+		view.putString(" " + pl.weapons.throwable.name, 0, row++, c.r, c.g, c.b);
+		view.putString(" Ammo: " + build("✒", pl.weapons.throwable.ammo), 0, row++, c.r, c.g, c.b);
 		++row
 		view.putString("[␣] " + (pl.drawn ? "Shoot" : "Punch"), 0, row++, 160, 0, 0);
 
